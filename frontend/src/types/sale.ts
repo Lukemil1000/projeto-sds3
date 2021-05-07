c1ae5996ddebca8cbd6c26afd68f5530a7b86557
Lucas Detogni Pomar <lucaspomar@outlook.com>
2021-05-07 18:50:43 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,5 @@
+import { Seller } from "./seller"
+
 export type SaleSum = {
     sum: number;
     sellerName: string;
@@ -7,4 +9,25 @@ export type SaleSuccess = {
     visited: number;
     deals: number;
     sellerName: string;
+}
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
 }
